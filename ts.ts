94c5bed954464d08c913be0e4e8d1f6ba6aed873
Tomasz Baran <tomasz.baran@microsoft.wsei.edu.pl>
2021-03-07 11:11:47 +0100

    To rework

--- ts.ts
+++ ts.ts
@@ -1,5 +1,7 @@
+/*
 class StartApp{
     containerDomElement: Element;
+
     constructor(containerDOMElement : Element){
         if(!containerDOMElement){
             throw new Error ("musisz podac pojemnik dla programy StartApp");
@@ -24,3 +26,30 @@ class Statystyka{
 }
 
 const startapp= new StartApp(document.body);
+*/
+
+let value1:string;
+let value2:string;
+let value3:string;
+let value4:string;
+
+const w1 = document.querySelector('input1');
+const w2 = document.querySelector('#input2');
+const w3 = document.querySelector('#input3');
+const w4 = document.querySelector('#input4');
+
+value1=(w1 as HTMLInputElement).value;
+value2=(w2 as HTMLInputElement).value;
+value3=(w3 as HTMLInputElement).value;
+value4=(w4 as HTMLInputElement).value;
+
+function suma(a:number,b:number,c:number,d:number){
+    return a+b+c+d;
+}
+
+let sumaInput= suma(parseInt(value1),parseInt(value2),parseInt(value3),parseInt(value4));
+
+const output=document.createElement('h1');
+output.innerText=String(sumaInput);
+
+console.log(sumaInput);
