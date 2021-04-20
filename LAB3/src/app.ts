7791e3b309b60e91b8ea19f3eb3a4e0451f1f57b
Tomasz Baran <tomasz.baran@microsoft.wsei.edu.pl>
2021-04-20 23:32:06 +0200

    Test03

--- LAB3/src/app.ts
+++ LAB3/src/app.ts
@@ -1,12 +1,32 @@
 export class App {
-    opwApiKey = '50d53005c0fd5f556bb4ef15224c4209';
+    opwApiKey = 'd4f238eb17f043fca2a79be91d062376';
     constructor() {
-        this.getCityInfo('zakopane')
+        this.onClickButton();
+        // this.getCityInfo('zakopane');
     }
+
+    onClickButton(){
+        const cityInput = <HTMLInputElement>document.getElementById('cityButton');
+        cityInput.addEventListener('click', (ev:Event) => this.getCityName())
+    }
+
+    //info z api dot CityName
+    getCityName(){
+        const cityInput = <HTMLInputElement>document.getElementById('cityName');
+        const city = cityInput.value;
+        console.log(city);
+        // wpisanie parametru do innej funkcji
+        this.getCityInfo(city);
+    }
+
+    //info z api dot weather
     async getCityInfo(city: string) {
-        const weather = await this.getWeather('zakopane');
+        const weather = await this.getWeather(city);
         this.saveData(weather);
+        const a= weather.wind.speed;
+        console.log(a);
     }
+
     async getWeather(city: string): Promise<any> {
         const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
         const weatherResponse = await fetch(openWeatherUrl);
@@ -14,9 +34,12 @@ export class App {
         console.log(weatherData);
         return weatherData;
     }
+
     saveData(data: any) {
+        //jakas petla do weatherData/ zmienna globalna do ilosci uzyc
         localStorage.setItem('weatherData', JSON.stringify(data));
     }
+
     getData() {
         const data = localStorage.getItem('weatherData');
         if (data) {
