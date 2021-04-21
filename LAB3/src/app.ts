b6fe4bd05e19bfed29631c4de733bd70deaebd94
PanWidmo <tomasz.baranww@gmail.com>
2021-04-21 21:33:30 +0200

    Adding new weather on page now works (without saving to memory)

--- LAB3/src/app.ts
+++ LAB3/src/app.ts
@@ -1,7 +1,7 @@
 export class App {
     opwApiKey = 'd4f238eb17f043fca2a79be91d062376';
+    boxNumber: number = 0;
     constructor() {
-        this.getCityInfo('zakopane')
         this.onClickButton();
         // this.getCityInfo('zakopane');
     }
@@ -17,15 +17,52 @@ export class App {
         const city = cityInput.value;
         console.log(city);
         //wpisanie parametru do innej funkcji
-        this.getCityInfo(city);
+        this.getCityWeather(city);
     }
 
     //tu info z api dot weather
-    async getCityInfo(city: string) {
+    async getCityWeather(city: string) {
         const weather = await this.getWeather(city);
+
+        //pobieranie danych z jsona
+        const name = weather.name;
+        const temp = Math.round(weather.main.temp - 273.15).toString();
+        const sky = weather.weather[0].main;
+        const pressure = weather.main.pressure;
+        const humidity = weather.main.humidity;
+
+        //tworzenie szkieletu
+        const weatherBox = document.createElement('div');
+        weatherBox.className = 'weatherBoxClass'
+        weatherBox.setAttribute("id","weatherBoxId");
+        const weatherCityName = document.createElement('h1');
+        const weatherCityTemp = document.createElement('p');
+        const weatherCitySky = document.createElement('p');
+        const weatherCityPressure = document.createElement('p');
+        const weatherCityHumidity = document.createElement('p');
+
+        //dodanie styli do szkieletu
+        weatherBox.classList.add('weatherBox');
+
+        //uzupelnienie szkieletu danymi
+        weatherCityName.innerHTML = name;
+        weatherCityTemp.innerHTML = "Temp: " + temp +"&deg;C";
+        weatherCitySky.innerHTML = sky;
+        weatherCityPressure.innerHTML = "Pressure: " + pressure + " hPA";
+        weatherCityHumidity.innerHTML = "Humidity: " + humidity + "%";
+
+        //wrzucenie szkieletu z danymi na strone
+        const space = document.getElementById('weathers');
+        space.appendChild(weatherBox);
+        weatherBox.appendChild(weatherCityName);
+        weatherBox.appendChild(weatherCityTemp);
+        weatherBox.appendChild(weatherCitySky);
+        weatherBox.appendChild(weatherCityPressure);
+        weatherBox.appendChild(weatherCityHumidity);
+
+        //zapisanie do pamieci
         this.saveData(weather);
-        const a= weather.wind.speed;
-        console.log(a);
+
     }
 
     async getWeather(city: string): Promise<any> {
@@ -36,11 +73,13 @@ export class App {
         return weatherData;
     }
 
+    //zapis do pamieci
     saveData(data: any) {
         //jakas petla do weatherData/ zmienna globalna do ilosci uzyc
         localStorage.setItem('weatherData', JSON.stringify(data));
     }
 
+    //wyciaganie z pamieci
     getData() {
         const data = localStorage.getItem('weatherData');
         if (data) {
