580fb7917e6366d23e8ff4130c7ad5d78ae04126
PanWidmo <tomasz.baranww@gmail.com>
2021-04-22 23:26:33 +0200

    Added: get data from local storage, automatic refresh weather status (1 min) from api, key 'enter' to adding by new city

--- LAB3/src/app.ts
+++ LAB3/src/app.ts
@@ -4,24 +4,41 @@ export class App {
 
     constructor() {
         this.onClickButton();
-        // this.getCityInfo('zakopane');
+        this.fetchFromStorage("T");
+        //odswiezanie co 1 min
+        setInterval(()=> this.fetchFromStorage("N"),60000);
     }
 
+    //dodanie miasta przez button lub enter
     onClickButton(){
-        const cityInput = <HTMLInputElement>document.getElementById('cityButton');
-        cityInput.addEventListener('click', (ev:Event) => this.getCityName())
+        const addByBtn = <HTMLInputElement>document.getElementById('cityButton');
+        addByBtn.addEventListener('click', (ev:Event) => this.getCityName());
+
+        const addByEnter = document.body;
+        addByEnter.addEventListener('keydown', (ev:KeyboardEvent) => {
+            if(ev.key === 'Enter'){
+                this.getCityName();
+            }
+        });
     }
 
-    //info z api dot CityName
+    //pobieranie danych z api i zwracanie obiektu weatherData (json)
+    async getWeather(city: string): Promise<any> {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        return weatherData;
+    }
+
+    //pobieranie nazwy miasta z inputa i wywolywanie getCityWeather
     getCityName(){
         const cityInput = <HTMLInputElement>document.getElementById('cityName');
         const city = cityInput.value;
-        //console.log(city);
-        //wpisanie parametru do innej funkcji
         this.getCityWeather(city);
     }
 
-    //tu info z api dot weather
+    //DO PODZIELENIA!
+    //tworzenie nowego okna pogodowego i zapis do local storage
     async getCityWeather(city: string) {
         const weather = await this.getWeather(city);
 
@@ -35,8 +52,8 @@ export class App {
 
         //tworzenie szkieletu
         const weatherBox = document.createElement('div');
-        weatherBox.className = 'weatherBoxClass'
-        weatherBox.setAttribute("id","weatherBoxId");
+        weatherBox.className = 'weatherBoxClass';
+        weatherBox.setAttribute("id","weatherBoxId" + this.boxNumber);
         const weatherCityName = document.createElement('h1');
         const weatherCityImg = document.createElement('img');
         const weatherCityTemp = document.createElement('p');
@@ -72,15 +89,78 @@ export class App {
 
     }
 
-    async getWeather(city: string): Promise<any> {
-        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
-        const weatherResponse = await fetch(openWeatherUrl);
-        const weatherData = await weatherResponse.json();
-        console.log(weatherData);
-        return weatherData;
+    fetchFromStorage(yesOrNo:string){
+        this.deleteWindows();
+        const tab = localStorage.length;
+
+        for (let i = 1; i < tab; i++) {
+            let nazwa;
+            nazwa = localStorage.getItem("weatherData" +i);
+            const nazwa2= JSON.parse(nazwa);
+            if(yesOrNo ==="T"){
+            this.getCityWeatherFromStorage(nazwa2,"T");
+            }
+            else{
+                this.getCityWeatherFromStorage(nazwa2,"N");
+            }
+        }
     }
 
-    //zapis do pamieci
+    //pobieranie z localStorage
+    async getCityWeatherFromStorage(nazwa:any,yesOrNo:string) {
+
+        const weather = await this.getWeather(nazwa.name);
+
+        //pobieranie danych z jsona
+        const name = weather.name;
+        const img =  `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+        const temp = Math.round(weather.main.temp - 273.15).toString();
+        const sky = weather.weather[0].main;
+        const pressure = weather.main.pressure;
+        const humidity = weather.main.humidity;
+
+        //tworzenie szkieletu
+        const weatherBox = document.createElement('div');
+        weatherBox.className = 'weatherBoxClass'
+        weatherBox.setAttribute("id","weatherBoxId" + this.boxNumber);
+        const weatherCityName = document.createElement('h1');
+        const weatherCityImg = document.createElement('img');
+        const weatherCityTemp = document.createElement('p');
+        const weatherCitySky = document.createElement('p');
+        const weatherCityPressure = document.createElement('p');
+        const weatherCityHumidity = document.createElement('p');
+
+        //dodanie styli do szkieletu
+        weatherBox.classList.add('weatherBox');
+
+        //uzupelnienie szkieletu danymi
+        weatherCityName.innerHTML = name;
+        weatherCityImg.src = img;
+        weatherCityTemp.innerHTML = "Temp: " + temp +"&deg;C";
+        weatherCitySky.innerHTML = "Sky: " + sky;
+        weatherCityPressure.innerHTML = "Pressure: " + pressure + " hPA";
+        weatherCityHumidity.innerHTML = "Humidity: " + humidity + "%";
+
+        //wrzucenie szkieletu z danymi na strone
+        const space = document.getElementById('weathers');
+        space.appendChild(weatherBox);
+        weatherBox.appendChild(weatherCityName);
+        weatherBox.appendChild(weatherCityImg);
+        weatherBox.appendChild(weatherCityTemp);
+        weatherBox.appendChild(weatherCitySky);
+        weatherBox.appendChild(weatherCityPressure);
+        weatherBox.appendChild(weatherCityHumidity);
+
+        //czyszczenie inputa
+        const cityInput = <HTMLInputElement>document.getElementById("cityName");
+        cityInput.value = "";
+        //jesli T to zapisuje do pamieci- N nie zapisuje
+        if(yesOrNo === "T")
+        this.saveData(weather);
+
+    }
+
+    //zapis do local storage
     saveData(data: any) {
         this.boxNumber++;
         localStorage.setItem('weatherData' + this.boxNumber, JSON.stringify(data));
@@ -94,5 +174,11 @@ export class App {
         } else {
             return {};
         }
+    }
+
+    //usuawnie okienek
+    deleteWindows(){
+        const div= document.getElementById('weathers');
+        div.textContent = "";
     }
 }
