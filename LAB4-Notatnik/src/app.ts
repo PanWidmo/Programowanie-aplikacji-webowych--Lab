ec13da5776daf32bad0b778f1d7dcf22798f52d6
PanWidmo <tomasz.baranww@gmail.com>
2021-05-30 10:08:21 +0200

    Proper fetch notes from localStorage

--- LAB4-Notatnik/src/app.ts
+++ LAB4-Notatnik/src/app.ts
@@ -1,18 +1,17 @@
 import {IAppStorage} from "./IAppStorage";
 export class App {
-    boxNumber: number = 0;
+    noteNumber: number = 0;
     notes: IAppStorage[] = [];
 
     constructor() {
         this.onClickButton();
-
-        //this.fetchFromStorage();
+        this.fetchNotesFromStorage();
     }
 
     //dodanie notatki przez button lub enter
     onClickButton(){
         const addByBtn = <HTMLInputElement>document.getElementById('addBtn');
-       addByBtn.addEventListener('click', (ev:Event) => this.addNewNote());
+        addByBtn.addEventListener('click', (ev:Event) => this.addNewNote());
 
         const addByEnter = document.body;
         addByEnter.addEventListener('keydown', (ev:KeyboardEvent) => {
@@ -22,6 +21,7 @@ export class App {
         });
     }
 
+    //szkielet notatki
     async addNewNote(){
         const noteTitle = <HTMLInputElement>document.getElementById('title');
         const title = noteTitle.value;
@@ -31,7 +31,7 @@ export class App {
         const color = colorName.value;
 
         const object: IAppStorage = {
-            id: this.boxNumber+1,
+            id: this.noteNumber+1,
             title: title,
             description: description,
             color: color,
@@ -43,16 +43,24 @@ export class App {
 
     }
 
+    //zapis do localStorage
     saveData(addNewNote: any) {
-        this.boxNumber++;
+        this.noteNumber++;
         this.notes.push(addNewNote);
-        localStorage.setItem("note"+this.boxNumber, JSON.stringify(addNewNote));
-        const result = JSON.parse(localStorage.getItem("note"+this.boxNumber));
+        localStorage.setItem("note"+this.noteNumber, JSON.stringify(addNewNote));
+        //sprawdzanie
+        const result = JSON.parse(localStorage.getItem("note"+this.noteNumber));
         console.log(result);
     }
 
-
+    //wyciaganie z localStorage
     fetchNotesFromStorage(){
+        const notesInStorage = localStorage.length;
+
+        for (let i = 1; i < notesInStorage; i++) {
+            localStorage.getItem("notes" +i);
+            this.addNewNote();
+        }
 
 
     }
