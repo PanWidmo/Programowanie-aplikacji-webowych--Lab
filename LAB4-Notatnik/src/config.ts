57a632d010c78f4e9252f3ce56429b7500b4ef17
PanWidmo <tomasz.baranww@gmail.com>
2021-06-10 21:29:52 +0200

    Connection with firebase- in progress (installed 'npm i firebase', created appFirebaseStorage)

---
+++ LAB4-Notatnik/src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAne8MofYpuW15WE85gr8Yj5CgrgbQKR5U",
+    authDomain: "webappslabs-notatnik.firebaseapp.com",
+    projectId: "webappslabs-notatnik",
+    storageBucket: "webappslabs-notatnik.appspot.com",
+    messagingSenderId: "511773634971",
+    appId: "1:511773634971:web:126a889864d99b3243f13e"
+  };
