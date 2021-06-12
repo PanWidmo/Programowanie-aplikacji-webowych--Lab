7c080b75459b990bfe74531716f3e2d5c3f64d8b
PanWidmo <tomasz.baranww@gmail.com>
2021-06-12 11:28:22 +0200

    Added method pinNote to appFirebaseStorage

--- LAB4-Notatnik/src/appFirebaseStorage.ts
+++ LAB4-Notatnik/src/appFirebaseStorage.ts
@@ -18,31 +18,25 @@ export class AppFirebaseStorage{
         this.db = this.firebaseApp.firestore();
     }
 
+    async addNote(note:IAppStorage) {
+        await this.db.collection('notatki').add(note);
+    }
 
-        async addNote(note:IAppStorage) {
-            await this.db.collection('notatki').add(note);
-            console.log("dodano notatke");
-        }
+    async deleteNote(id: string){
+        await this.db.collection('notatki').doc(id).delete();
+    }
 
-        async deleteNote(id: string){
-            await this.db.collection('notatki').doc(id).delete();
-        }
+    async pinNote(id: string, note: IAppStorage){
+        await this.db.collection('notes').doc(id).update(note);
+    }
 
-        async updateNote(id: string, note: IAppStorage){
-            await this.db.collection('notatki').doc(id).update(note);
-        }
+    async getNote(id:string){
+        return await this.db.collection('notatki').doc(id).get().then((res:any) => res.data());
+    }
 
-        async getNote(id:string){
-            console.log("getNote");
-            return await this.db.collection('notatki').doc(id).get().then((res:any) => res.data());
-        }
-
-        async getNotes(){
-            console.log("getNotes");
-            const collection = await this.db.collection('notatki').get().then((res: any) => ({size: res.size, docs: res.docs}));
-            return collection.docs.map((doc: any) => ({id: doc.id, data: doc.data()}));
-        }
-
-
+    async getNotes(){
+        const collection = await this.db.collection('notatki').get().then((res: any) => ({size: res.size, docs: res.docs}));
+        return collection.docs.map((doc: any) => ({id: doc.id, data: doc.data()}));
+    }
 
 }
