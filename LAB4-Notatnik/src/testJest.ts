eb42c769d0c3e165e619887c0a9b19f8ff7b1825
PanWidmo <tomasz.baranww@gmail.com>
2021-06-25 20:27:10 +0200

    5x Unit Tests- done

--- LAB4-Notatnik/src/testJest.ts
+++ LAB4-Notatnik/src/testJest.ts
@@ -3,4 +3,24 @@ export class TestJest {
     sumFunction(x: any, y: any) {
         return x + y;
     }
+
+    diffFunction(x: any, y: any){
+        return x-y;
+    }
+
+    divisionFunction(x: any, y: any){
+        return x/y;
+    }
+
+    multiplicationFunction(x: any, y: any){
+        return x*y;
+    }
+
+    isEqualFunction(x: any, y: any){
+        if(x==y){
+            return true;
+        }
+        else return false;
+    }
+
 }
--- LAB4-Notatnik/unitTests/index.test.ts
+++ LAB4-Notatnik/unitTests/index.test.ts
@@ -2,7 +2,7 @@ import {TestJest} from '../src/testJest';
 import { IAppStorage } from '../src/IAppStorage';
 import { AppFirebaseStorage } from '../src/appFirebaseStorage';
 
-describe('CalculatorTests', () => {
+describe('Calculate Tests', () => {
     const calculate = new TestJest();
 
     it('Sum Function', () => {
@@ -11,6 +11,31 @@ describe('CalculatorTests', () => {
 
         const tmp = calculate.sumFunction(a,b);
         expect(tmp).toBe(10);
+    });
+    it('Diff Function', () => {
+        const a = 10;
+        const b = 2;
+
+        const tmp = calculate.diffFunction(a,b);
+        expect(tmp).toBe(8);
+    });
+    it('Division Function', () => {
+        const a = 10;
+        const b = 5;
+        const tmp = calculate.divisionFunction(a,b);
+        expect(tmp).toBe(2);
+    });
+    it('Multiplication Function', () => {
+        const a= 3 ;
+        const b = 3;
+        const tmp = calculate.multiplicationFunction(a,b);
+        expect(tmp).toBe(9);
+    });
+    it('Equal Check Function', () => {
+        const a = 1;
+        const b = 2;
+        const tmp = calculate.isEqualFunction(a,b);
+        expect(tmp).toBe(false);
     })
 })
 
